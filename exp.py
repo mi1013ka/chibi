e418efed3d02e0df0f68986b8cb6ec0315b4e2bd
SEGAWA Mika <21716055@jwu.ac.jp>
2019-10-14 16:21:00 +0900

    Expr

--- exp.py
+++ exp.py
@@ -1,4 +1,7 @@
-class Val(object):
+class Expr(object):
+    pass
+
+class Val(Expr):
     __slots__=['value']
     def __init__(self,value=0):
         self.value=value
@@ -13,7 +16,11 @@ v=Val(1)
 print(v)
 assert v.eval() == 1
 
-class Add(object):
+assert isinstance(v, Expr)  # ==> True
+assert isinstance(v, Val)   # ==> True
+assert isinstance(v, int)   # ==>False
+
+class Add(Expr):
     __slots__=['left', 'right']
     def __init__(self,a,b):
         self.left=a
