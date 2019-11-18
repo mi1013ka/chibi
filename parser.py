2112b5de38772f5844dc76fcb8e640dd85639e9f
SEGAWA Mika <21716055@jwu.ac.jp>
2019-11-18 16:22:09 +0900

    output binary Add

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ from exp import Val, Add
 def parse(s: str):
     num=int(s)
     return Val(num)
-
+"""
 e =parse("123")
 print(e)
 
@@ -16,3 +16,13 @@ print("pos", pos)
 s1=s[0:pos]
 s2=s[pos+1:]
 print(s1,s2)
+"""
+
+def parse(s: str):
+    pos = s.find("+")
+    if pos == -1:
+        num = int(s)
+        return Add(Val(int(s1)), Val(int(s2)))
+
+e = parse("123+456")
+print(e)
