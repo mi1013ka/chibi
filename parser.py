2bf9d1526c9848cbe3eef60f77677ea4b3cd1c9e
SEGAWA Mika <21716055@jwu.ac.jp>
2019-11-18 16:46:01 +0900

    output Add

--- parser.py
+++ parser.py
@@ -1,28 +1,12 @@
 from exp import Val, Add
 def parse(s: str):
-    num=int(s)
-    return Val(num)
-"""
-e =parse("123")
-print(e)
-
-s="1+2"
-print(e)
-
-s="1+2"
-pos=s.find("+")
-print("pos", pos)
-
-s1=s[0:pos]
-s2=s[pos+1:]
-print(s1,s2)
-"""
-
-def parse(s: str):
-    pos = s.find("+")
+    pos =s.find('+')
     if pos == -1:
-        num = int(s)
-        return Add(Val(int(s1)), Val(int(s2)))
+        return Val(int(s))
+    else:
+        s1=s[0:pos]
+        s2=s[pos+1:]
+        return Add(parse(s1), parse(s2))
 
-e = parse("123+456")
-print(e)
+e =parse("1+2+3")
+print(e, e.eval())
